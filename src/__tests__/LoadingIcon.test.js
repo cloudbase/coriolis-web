265c679032496cc53668e330cd0cf1a6ef8b93de
George Vrancianu <gvrancianu@cloudbasesolutions.com>
2017-07-28 17:13:20 +0300

    LoadingIcon snapshot tests

--- src/__tests__/LoadingIcon.test.js
+++ src/__tests__/LoadingIcon.test.js
@@ -9,4 +9,25 @@ describe('Loading icon', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('renders correctly animated', () => {
+    const tree = renderer.create(
+      <LoadingIcon animate />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with text', () => {
+    const tree = renderer.create(
+      <LoadingIcon text="Test text" />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with sizes', () => {
+    const tree = renderer.create(
+      <LoadingIcon width={300} height={200} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 })
