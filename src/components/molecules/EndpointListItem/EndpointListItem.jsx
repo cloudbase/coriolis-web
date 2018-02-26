df2ccc6973aed2242a2ce40ea605cbe316e1736c
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2018-02-26 13:10:15 +0200

    The last endpoint in the list has no bottom border

--- src/components/molecules/EndpointListItem/EndpointListItem.jsx
+++ src/components/molecules/EndpointListItem/EndpointListItem.jsx
@@ -27,14 +27,6 @@ const CheckboxStyled = styled(Checkbox) `
   opacity: ${props => props.checked ? 1 : 0};
   transition: all ${StyleProps.animations.swift};
 `
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-
-  &:hover ${CheckboxStyled} {
-    opacity: 1;
-  }
-`
 const Content = styled.div`
   display: flex;
   align-items: center;
@@ -48,6 +40,18 @@ const Content = styled.div`
 
   &:hover {
     background: ${Palette.grayscale[1]};
+  }
+`
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  &:hover ${CheckboxStyled} {
+    opacity: 1;
+  } }
+
+  &:last-child ${Content} {
+    border-bottom: 1px solid ${Palette.grayscale[1]};
   }
 `
 const Image = styled.div`
