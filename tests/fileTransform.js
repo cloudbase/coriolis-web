79bfba12c77acc469e2c7e68454ac5d18f576fee
Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>
2025-05-05 11:31:58 +0000

    Update test configuration for `jest` dependency version 29+
    Signed-off-by: Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>

--- tests/fileTransform.js
+++ tests/fileTransform.js
@@ -1,6 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  process: (_, filename) =>
-    `module.exports = ${JSON.stringify(path.basename(filename))}`,
+  process(_, filename) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(filename))};`,
+    };
+  },
+  getCacheKey() {
+    return "fileTransform";
+  },
 };
