d83c8565250f3a542fc153af1214ed76d04bc246
Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>
2025-05-29 07:21:25 +0000

    Modularize `VHI components` to extend Openstack plugins
    Signed-off-by: Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>

--- src/plugins/vhi/MinionPoolSchemaPlugin.ts
+++ src/plugins/vhi/MinionPoolSchemaPlugin.ts
@@ -1,23 +1,3 @@
-import MinionPoolSchemaPluginBase from "@src/plugins/default/MinionPoolSchemaPlugin";
-import { Field } from "@src/@types/Field";
-import DomUtils from "@src/utils/DomUtils";
+import OpenstackMinionPoolSchemaPlugin from "@src/plugins/openstack/MinionPoolSchemaPlugin";
 
-export default class MinionPoolSchemaPlugin extends MinionPoolSchemaPluginBase {
-  override getMinionPoolToOptionsQuery(envData: any) {
-    return `?env=${DomUtils.encodeToBase64Url({
-      ...envData,
-      list_all_destination_networks: true,
-    })}`;
-  }
-
-  override minionPoolTransformOptionsFields(fields: Field[]) {
-    // Remove this field, as all networks are always listed
-    fields = fields.filter(f => f.name !== "list_all_destination_networks");
-    return fields;
-  }
-
-  override getMinionPoolEnv(schema: Field[], data: any) {
-    const payload: any = super.getMinionPoolEnv(schema, data);
-    return { ...payload, list_all_destination_networks: true };
-  }
-}
+export default class VHIMinionPoolSchemaPlugin extends OpenstackMinionPoolSchemaPlugin {}
