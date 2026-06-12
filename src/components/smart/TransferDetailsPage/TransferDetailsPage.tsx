2a44c7dbfaa95022c5919d915f84f889ea6ccb40
Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>
2026-06-12 13:02:36 +0300

    Populate instance details after execution without a refresh
    Polling refreshed `transfer.info` once an execution
    collected the VM `export_info` but never re-derived
    the instance details, so after executing a transfer
    those sections stayed empty until a manual page refresh.
    
    Signed-off-by: Mihaela Balutoiu <mbalutoiu@cloudbasesolutions.com>

--- src/components/smart/TransferDetailsPage/TransferDetailsPage.tsx
+++ src/components/smart/TransferDetailsPage/TransferDetailsPage.tsx
@@ -365,6 +365,17 @@ class TransferDetailsPage extends React.Component<Props, State> {
     this.setState({ dbInstancesDetails: instancesDetails });
   }
 
+  populateInstanceDetails() {
+    const transfer = this.transfer;
+    if (
+      transfer &&
+      !this.state.dbInstancesDetails.length &&
+      this.hasStoredVmInfo(transfer.info)
+    ) {
+      this.populateInstanceStoreFromTransferInfo(transfer.info);
+    }
+  }
+
   isExecuteDisabled() {
     const transfer = this.transfer;
     if (!transfer) {
@@ -471,10 +482,12 @@ class TransferDetailsPage extends React.Component<Props, State> {
   }
 
   handleCreateDeploymentClick() {
+    this.populateInstanceDetails();
     this.setState({ showDeploymentModal: true, pausePolling: true });
   }
 
   handleTransferEditClick() {
+    this.populateInstanceDetails();
     this.setState({ showEditModal: true, pausePolling: true });
   }
 
@@ -633,6 +646,7 @@ class TransferDetailsPage extends React.Component<Props, State> {
         }
       })(),
     ]);
+    this.populateInstanceDetails();
 
     setTimeout(() => {
       this.pollData();
